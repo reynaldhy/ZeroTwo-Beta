50027ebef4b0ac3027786ac63db3207554f4740c
ZeroTwo <50521661+reynaldhy@users.noreply.github.com>
2026-02-12 00:15:06 +0700

    Add Firebase configuration and initialization

---
+++ Firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase CDN
+const firebaseConfig = {
+  apiKey: "AIzaSyDQRaWCakzjbHU1Oc6Fol2b3Be_UPSs5lg",
+  authDomain: "osint-pro-02.firebaseapp.com",
+  projectId: "osint-pro-02",
+  storageBucket:"osint-pro-02.firebasestorage.app",
+  messagingSenderId: "502464941682",
+  appId: "1:502464941682:web:3e043111a367017120bceb"
+};
+
+// Initialize
+firebase.initializeApp(firebaseConfig);
